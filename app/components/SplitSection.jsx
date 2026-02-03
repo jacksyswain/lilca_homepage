e8e1990fb27ebaa75c9617bfed0475f2a7fc373b
jyotiprakash swain <jp.swainabc@gmail.com>
2026-02-03 21:46:24 +0530

    new update

--- app/components/SplitSection.jsx
+++ app/components/SplitSection.jsx
@@ -1,21 +1,3 @@
-export default function SplitSection() {
-  return (
-    <section className="grid grid-cols-1 lg:grid-cols-2">
-      <div className="h-[500px] bg-sand"></div>
-
-      <div className="bg-[#d6d3dc] p-20">
-        <h2 className="font-serif text-3xl mb-6">
-          You don’t have to do this alone.
-        </h2>
-        <ul className="text-sm space-y-3">
-          <li>• Anxiety & overwhelm</li>
-          <li>• Life transitions</li>
-          <li>• Emotional balance</li>
-        </ul>
-      </div>
-    </section>
-  );
-}
 import RevealItem from "./RevealItem";
 
 export default function SplitSection() {
